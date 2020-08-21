c3d9d9ed113448324959874bba4e56d9b5cd6390
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-08-21 13:38:43 +0300

    Create branch lesson04

--- script/script.js
+++ script/script.js
@@ -1,42 +1,12 @@
-let lang = 'en'; // предполагаем, что возможно только одно из 2-х значений: 'ru' или 'en'
-let weekDaysRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-let weekDaysEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+'use strict';
 
-// task 1a:
-console.log('  with IF:');
-if (lang === 'ru')
-  console.log(weekDaysRu.toString());
-else // if (lang === 'en')
-  console.log(weekDaysEn.toString());
-
-// task 1b:
-console.log('  with SWITCH:');
-switch (lang) {
-  case 'ru':
-    console.log(weekDaysRu.toString());
-    break;
-  case 'en':
-    console.log(weekDaysEn.toString());
-    break;
-    //default:
+function testFunc(str) {
+  if (typeof str !== "string") {
+    alert('Тип полученного аргумента не является строкой!');
+    return null;
+  }
+  let strTrim = String(str).trim();
+  return strTrim.length > 30 ? strTrim.substr(0, 30) + '...' : strTrim;
 }
 
-// task 1c:
-console.log('  with OBJECT:');
-let weekDays = {
-  'ru': weekDaysRu,
-  'en': weekDaysEn
-};
-let weekDaysString = '';
-for (let i = 0; i < 7; i++)
-  weekDaysString += weekDays[lang][i] + ',';
-console.log(weekDaysString.substr(0, weekDaysString.length - 1));
-
-// task 2:
-let namePerson = prompt('Введите имя');
-if (namePerson === null)
-  namePerson = '';
-let namePersonUpCase = namePerson.toUpperCase();
-console.log(namePersonUpCase === 'АРТЕМ' ? 'директор' :
-  namePersonUpCase === 'МАКСИМ' ? 'преподаватель' :
-  'студент');
+console.log(testFunc('012345678901234567890123456789'));
